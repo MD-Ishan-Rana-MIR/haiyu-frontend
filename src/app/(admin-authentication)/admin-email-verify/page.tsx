994d7f333c401fc21cb14bab236330d88b311dd5
ishanrana363 <ishanrana363@gmail.com>
2025-08-04 10:02:27 +0600

    admin login email verify page design completed

---
+++ src/app/(admin-authentication)/admin-email-verify/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminEmailVerify from './AdminEmailVerify'
+
+const Page : React.FC = () => {
+  return (
+    <div>
+        <AdminEmailVerify/>
+    </div>
+  )
+}
+
+export default Page
